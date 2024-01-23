075815c36ea502d71e83b253614a5bc0eb2b0121
baylee <bcaldwel@my.athens.edu>
2024-01-23 16:45:22 -0600

    Update asg2p3.cpp

--- assignment 2/asg2p3.cpp
+++ assignment 2/asg2p3.cpp
@@ -2,3 +2,18 @@
 #include <vector>
 #include <iostream>
 
+template <typename Thing>
+class ReceiptBag {
+private:
+    std::vector<Thing> items;
+    std::vector<int> receipts;
+    int nextReceipt;
+
+public:
+    ReceiptBag();
+    int insert(Thing aThing);
+    Thing& pop(int receipt);
+    int size();
+    int count();
+};
+
