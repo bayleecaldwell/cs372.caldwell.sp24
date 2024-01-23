735263a8fd29094d69011f7433588541a9c63738
baylee <bcaldwel@my.athens.edu>
2024-01-22 21:00:34 -0600

    Update asg2p2.cpp

--- assignment 2/asg2p2.cpp
+++ assignment 2/asg2p2.cpp
@@ -2,3 +2,14 @@
 
 template <typename T, size_t Size>
 class Array {
+public:
+    Array();
+    ~Array();
+    T& operator[](size_t index);
+    size_t size() const;
+    void traverse() const;
+
+private:
+    T arr[Size];
+};
+
