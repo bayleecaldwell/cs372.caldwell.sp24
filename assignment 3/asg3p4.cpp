3f6bef221af943a5053c1f1bc05345c0104045b7
baylee <bcaldwel@my.athens.edu>
2024-01-30 18:36:16 -0600

    Update asg3p4.cpp

--- assignment 3/asg3p4.cpp
+++ assignment 3/asg3p4.cpp
@@ -6,3 +6,28 @@ struct PriorityNode {
     int priority;
 };
 
+template <typename T>
+class List {
+private:
+    class Node {
+    public:
+        T data;
+        Node* prev;
+        Node* next;
+    };
+    Node* head;
+    Node* tail;
+
+public:
+    List();
+    List(T newData);
+    List(const List& rhs);
+    ~List();
+    void insert(Node* insertPoint, T data);
+    bool empty();
+    void push_front(T data);
+    void push_back(T data);
+    void pop_back();
+    void pop_front();
+    void traverse(void (*doIt)(T data));
+};
