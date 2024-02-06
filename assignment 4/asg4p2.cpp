893fb8a4d93525fddbb0b638939e86345a2e0787
baylee <bcaldwel@my.athens.edu>
2024-02-06 12:43:18 -0600

    Update asg4p2.cpp

--- assignment 4/asg4p2.cpp
+++ assignment 4/asg4p2.cpp
@@ -20,3 +20,17 @@ public:
     virtual T pop() = 0;
     virtual T& operator[](int index) = 0;
 };
+template<typename T>
+class Vector : public SequentialContainer<T> {
+public:
+    Vector() : SequentialContainer<T>() {}
+    ~Vector() {}
+
+    void push(const T& value) override {
+        T* newData = new T[this->size + 1];
+        for (int i = 0; i < this->size; ++i)
+            newData[i] = this->data[i];
+        newData[this->size++] = value;
+        delete[] this->data;
+        this->data = newData;
+    }
