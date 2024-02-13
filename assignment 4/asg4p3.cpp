a333d76cf5f9dbafdebe6fa3d36c881edd2e60dc
baylee <bcaldwel@my.athens.edu>
2024-02-13 09:22:33 -0600

    Update asg4p3.cpp

--- assignment 4/asg4p3.cpp
+++ assignment 4/asg4p3.cpp
@@ -3,3 +3,41 @@
 #include <string>
 #include <cctype>
 
+using namespace std;
+
+bool isOperator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+string infixToPostfix(string expression) {
+    stack<char> operators;
+    string postfix;
+
+    for (char c : expression) {
+        if (isdigit(c)) {
+            postfix += c;
+        } else if (c == '(') {
+            operators.push(c);
+        } else if (c == ')') {
+            while (!operators.empty() && operators.top() != '(') {
+                postfix += operators.top();
+                operators.pop();
+            }
+            operators.pop(); // Discard '('
+        } else if (isOperator(c)) {
+            while (!operators.empty() && operators.top() != '(' && ((c != '*' && c != '/') || (operators.top() == '+' || operators.top() == '-'))) {
+                postfix += operators.top();
+                operators.pop();
+            }
+            operators.push(c);
+        }
+    }
+
+    // Pop remaining operators
+    while (!operators.empty()) {
+        postfix += operators.top();
+        operators.pop();
+    }
+
+    return postfix;
+}
