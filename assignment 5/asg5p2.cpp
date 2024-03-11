917bdcc265ca4aed60275ed913cad9a73fa42d62
baylee <bcaldwel@my.athens.edu>
2024-03-11 18:53:42 -0500

    Update asg5p2.cpp

--- assignment 5/asg5p2.cpp
+++ assignment 5/asg5p2.cpp
@@ -46,3 +46,21 @@ public:
         std::cout << std::endl;
     }
 };
+
+int main() {
+
+    BinaryTree<int> tree;
+
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(2);
+    tree.insert(4);
+    tree.insert(6);
+    tree.insert(8);
+
+    std::cout << "Binary Tree: ";
+    tree.display();
+
+    return 0;
+}
