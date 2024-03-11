6a56c2ac9cddc6099026121cbe716686358f24c8
baylee <bcaldwel@my.athens.edu>
2024-03-11 18:30:26 -0500

    Update asg5p2.cpp

--- assignment 5/asg5p2.cpp
+++ assignment 5/asg5p2.cpp
@@ -1,3 +1,12 @@
 #include <memory>
+#include <iostream>
 
 template<typename T>
+class TreeNode {
+public:
+    T data;
+    std::shared_ptr<TreeNode<T>> left;
+    std::shared_ptr<TreeNode<T>> right;
+
+    TreeNode(T value) : data(value), left(nullptr), right(nullptr) {}
+};
