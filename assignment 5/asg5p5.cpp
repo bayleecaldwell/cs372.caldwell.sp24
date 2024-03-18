08cd1c753260f95d807c55459cd6f58944fd8a66
baylee <bcaldwel@my.athens.edu>
2024-03-18 11:13:09 -0500

    Update asg5p5.cpp

--- assignment 5/asg5p5.cpp
+++ assignment 5/asg5p5.cpp
@@ -11,3 +11,33 @@ public:
 
     Pair(T1 f, T2 s) : first(f), second(s) {}
 };
+
+template <typename KeyType, typename ValueType>
+class TreeNode {
+public:
+    Pair<KeyType, ValueType> data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(KeyType key, ValueType value) : data(key, value), left(nullptr), right(nullptr) {}
+};
+
+template <typename KeyType, typename ValueType>
+class Dictionary {
+private:
+    TreeNode<KeyType, ValueType>* root;
+
+    void insertNode(TreeNode<KeyType, ValueType>* &node, KeyType key, ValueType value) {
+        if (node == nullptr) {
+            node = new TreeNode<KeyType, ValueType>(key, value);
+            return;
+        }
+
+        if (key < node->data.first) {
+            insertNode(node->left, key, value);
+        } else if (key > node->data.first) {
+            insertNode(node->right, key, value);
+        } else {
+            node->data.second = value;
+        }
+    }
