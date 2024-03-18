39d7be4882c992e12e437f766bc49657428f0d35
baylee <bcaldwel@my.athens.edu>
2024-03-18 13:52:42 -0500

    Update asg5p4.cpp

--- assignment 5/asg5p4.cpp
+++ assignment 5/asg5p4.cpp
@@ -1,1 +1,12 @@
-#include
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+struct TreeNode {
+    T data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(T value) : data(value), left(nullptr), right(nullptr) {}
+};
