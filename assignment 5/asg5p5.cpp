538cefd78c977f45bc46360cc01d64046245fc39
baylee <bcaldwel@my.athens.edu>
2024-03-18 11:10:38 -0500

    Update asg5p5.cpp

--- assignment 5/asg5p5.cpp
+++ assignment 5/asg5p5.cpp
@@ -2,3 +2,12 @@
 #include <list>
 
 using namespace std;
+
+template <typename T1, typename T2>
+class Pair {
+public:
+    T1 first;
+    T2 second;
+
+    Pair(T1 f, T2 s) : first(f), second(s) {}
+};
