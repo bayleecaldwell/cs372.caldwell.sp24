59faea365efafd4ad28744e1fd133e212b3733b0
baylee <133932148+bayleecaldwell@users.noreply.github.com>
2024-04-16 19:54:37 -0500

    Update asg9p2.cpp

--- assignment 9/asg9p2.cpp
+++ assignment 9/asg9p2.cpp
@@ -17,16 +17,12 @@ public:
         table = new RecordType[TABLE_SIZE];
         numElements = 0;
         for (int i = 0; i < TABLE_SIZE; ++i) {
-            table[i].key = -1; 
+            table[i].key = -1;
         }
     }
 
     ~Table() {
         delete[] table;
-    }
-
-    void find(int key, bool &found, RecordType &result, std::function<void(int, bool &, std::size_t)> hashingFunction = linearProbe) {
-        hashingFunction(key, found, result);
     }
 
     void linearProbe(int key, bool &found, RecordType &result) {
@@ -71,6 +67,10 @@ public:
         found = false;
     }
 
+    void find(int key, bool &found, RecordType &result, void (Table::*hashingFunction)(int, bool &, RecordType &) = &Table::linearProbe) {
+        (this->*hashingFunction)(key, found, result);
+    }
+
     void insert(int key, const RecordType &record) {
         bool found;
         RecordType result;
@@ -80,7 +80,6 @@ public:
             numElements++;
         }
     }
-
 };
 
 int main() {
@@ -96,11 +95,11 @@ int main() {
     std::cout << "Linear Probing - Found: " << found << ", Key: " << result.key << std::endl;
 
     table.insert(22, record2);
-    table.find(22, found, result, table.quadraticProbe);
+    table.find(22, found, result, &Table::quadraticProbe);
     std::cout << "Quadratic Probing - Found: " << found << ", Key: " << result.key << std::endl;
 
     table.insert(33, record3);
-    table.find(33, found, result, table.secondHash);
+    table.find(33, found, result, &Table::secondHash);
     std::cout << "Second Hash Probing - Found: " << found << ", Key: " << result.key << std::endl;
 
     return 0;
